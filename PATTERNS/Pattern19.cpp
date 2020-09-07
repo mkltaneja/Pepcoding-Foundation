731f8fbce36910340ca023e2420a5715ad7be7b7
MUKUL <mkltaneja@gmail.com>
2020-09-07 19:30:01 +0530

    Pattern 19

---
+++ PATTERNS/Pattern19.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i == 0 && j <= n/2)
+                cout<<"*\t";
+            
+            else if(i < n/2)
+            {
+                if(j != n/2 && j != n-1)
+                    cout<<"\t";
+                else
+                    cout<<"*\t";
+            }
+            else if(i == n/2)
+                cout<<"*\t";
+            else if(i == n-1 && j >= n/2)
+                cout<<"*\t";
+            else if(i > n/2)
+            {
+                if(j != n/2 && j != 0)
+                    cout<<"\t";
+                else
+                    cout<<"*\t";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
